72b291b4259e2163c6d993a499b5363047d32adb
Ran Tao <ran.tao.aiuos@gmail.com>
2018-06-14 21:31:10 -0700

    Create 5 htol.c

---
+++ 5 htol.c
@@ -0,0 +1,14 @@
+void htol(char s[])
+{
+    for(int i = 0; i<strlen(s);++i)
+        if(s[i]>='A' && s[i]<='Z')
+            s[i]=s[i]-'A'+'a';
+}
+
+int main()
+{
+    char s[] = "ABcd+";
+    htol(s);
+    printf("%s",s);
+    return 0;
+}
