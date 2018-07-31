4d19e86bffdf18a9a8605b583d15d1df006347db
Ran Tao <ran.tao.aiuos@gmail.com>
2018-07-31 10:21:50 -0700

    Update 13 ITERATORS.js

--- 13 ITERATORS.js
+++ 13 ITERATORS.js
@@ -20,7 +20,7 @@ let onlyNumbers = things.filter(function(thing) {
 
 console.log(onlyNumbers);
 
-
+//----------------------------------------------------------------------------
 let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
@@ -29,3 +29,20 @@ fruits.forEach(function(fruit){
 });
 // using arrow function syntax
 fruits.forEach(fruit => console.log('I want to eat a '+fruit));
+
+//----------------------------------------------------------------------------
+let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+
+let nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+let longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a new array
+let smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
